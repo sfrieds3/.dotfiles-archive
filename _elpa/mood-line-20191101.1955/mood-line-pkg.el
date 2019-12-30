6407b52ee16a297a5c4d1803ef8c89da7d846005
scottf <tnp@thisnullptr.com>
2019-12-30 05:33:19 -0600

    Add elpa folder for emacs, update init.el

---
+++ _elpa/mood-line-20191101.1955/mood-line-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mood-line" "20191101.1955" "A minimal mode-line inspired by doom-modeline." '((emacs "24.4")) :commit "0b07bdc7e77e19d7c722c2db803be0d3b47aa692" :keywords '("mode-line" "faces") :authors '(("Jessie Hildebrandt <jessieh.net>")) :maintainer '("Jessie Hildebrandt <jessieh.net>") :url "https://gitlab.com/jessieh/mood-line")
